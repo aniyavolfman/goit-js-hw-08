dfc59411ee1a88cbc0d45a72b1667dc13ca0412e
Anna Volfman <aniya_07@ukr.net>
2022-12-25 20:46:43 +0200

    updates homework

---
+++ src/js/02-video.js
@@ -0,0 +1,26 @@
+import throttle from 'lodash.throttle';
+import Vimeo from '@vimeo/player';
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+player.on('timeupdate', throttle(onVideoPlayer, 1000));
+
+function onVideoPlayer (event) {
+    let dataSrtring = event.seconds;
+    localStorage.setItem(STORAGE_KEY, dataSrtring);
+}
+
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
